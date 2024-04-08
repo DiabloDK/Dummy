b4ad8f83030f35f0a34d532873f3d5fa8f3892d4
Diablo_DK <deepadharshan251@outlook.com>
2024-04-08 12:40:28 +0530

    Complete

--- Dijkstra.py
+++ Dijkstra.py
@@ -8,6 +8,12 @@ def dijkstra(graph, source):
     
     visited = set()
     
+    def redraw_table(node):
+        for i in range(num_nodes):
+            if distances[node] + graph[node][i] < distances[i]:
+                distances[i] = distances[node] + graph[node][i]
+                shortest_paths[i] = shortest_paths[node] + [i]
+    
     while True:
         min_distance = float('infinity')
         min_node = None
@@ -28,6 +34,8 @@ def dijkstra(graph, source):
                 if distance < distances[neighbor]:
                     distances[neighbor] = distance
                     shortest_paths[neighbor] = shortest_paths[min_node] + [neighbor]
+        
+        redraw_table(min_node)  # Call redraw_table function when a node fails
     
     return distances, shortest_paths
 
@@ -42,7 +50,6 @@ def create_graph():
     for _ in range(num_edges):
         start, end, weight = map(int, input("Enter edge (start end weight): ").split())
         graph[start][end] = weight
-        graph[end][start] = weight
     
     return graph
 
@@ -54,4 +61,21 @@ shortest_distances, shortest_paths = dijkstra(graph, source_node)
 print("Shortest distances from node", source_node, ": ", shortest_distances)
 
 for node, path in enumerate(shortest_paths):
+    print("Shortest path to node", node, ": ", path)
+
+# User specifies the node that fails
+failed_node = int(input("Enter the node that fails: "))
+
+# Modify the graph accordingly
+for i in range(len(graph)):
+    graph[failed_node][i] = 0
+    graph[i][failed_node] = 0
+
+# Rerun Dijkstra's algorithm with the updated graph
+new_shortest_distances, new_shortest_paths = dijkstra(graph, source_node)
+
+print("After node", failed_node, "fails:")
+print("Shortest distances from node", source_node, ": ", new_shortest_distances)
+
+for node, path in enumerate(new_shortest_paths):
     print("Shortest path to node", node, ": ", path)
